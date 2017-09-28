df29bd4553f710fdcf7d7466c2f1773f21c8b7d1
yang.wei <yang.wei@datatom.com>
2017-09-28 17:15:14 +0800

    feat:add cmatrix

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
-### __The road to professional-javascript development__  
-#### 1. _data-binding_  
->弄懂常见框架双向数据绑定的原理，尝试自己在代码中实现。  
-#### 2. _inheritation_  
+### __The road to JS-pro developer__
+
+#### 1. _data-binding_
+
+>弄懂常见框架双向数据绑定的原理，尝试自己在代码中实现  
+
+#### 2. _inheritation_
+
 >javascript常见的继承方式的实现，以及分析每种方法的执行过程和优劣  
-#### 3._design-patterns_  
+
+#### 3._design-patterns_
+
 >常用设计模式的应用和总结  
-#### 4._es2015-learning_  
+
+#### 4._es2015-learning_
+
 >es2015的学习  
-#### 5._front-end-rules_  
+
+#### 5._front-end-rules_
+
 >前端编码规范  
-#### 6._normal_  
->日常代码练习和知识点理解  
-#### 7._functional-programming_  
+
+#### 6._normal_
+
+>[1]cmatrix - 矩阵弹幕特效  
+>[2]codeWars - 在线编程网站  
+>[3]immutable - js对象深拷贝和浅拷贝框架  
+>[4]js-timer - js定时器  
+>[5]mouseDragDrop - 元素拖动demo  
+>[6]nowcoder - 牛客在线编程网站  
+>[7]promise - es6 异步解决方案  
+>[8]regular-expression - 正则表达式  
+>[9]textTips - 文本提示跟随demo  
+>[10]tools - 公用工具方法  
+
+
+#### 7._functional-programming_
+
 >js函数式编程学习  
--- normal/cmatrix/main.js
+++ normal/cmatrix/main.js
@@ -1,15 +1,112 @@
-/*1:声明变量*/
-let num1, num2;
-/*2：向屏幕输出，提示信息，要求输入num1*/
-process.stdout.write('请输入num1的值：');
-/*3：监听用户的输入*/
-process.stdin.on('data', function (chunk) {
-    if (!num1) {
-        num1 = Number(chunk);
-        /*4：向屏幕输出，提示信息，要求输入num2*/
-        process.stdout.write('请输入num2的值');
-    } else {
-        num2 = Number(chunk);
-        process.stdout.write('结果是：' + (num1 + num2));
+/* ************************* 页面初始化 ************************* */
+var matrixFather = document.querySelector('.matrix-border');
+// clear child
+var childNodes = matrixFather.childNodes;
+var length = childNodes.length;
+for (var i = 0; i < length; i++) {
+  matrixFather.removeChild(childNodes[0]);
+}
+/* ************************* 享元模式管理矩阵 ************************* */
+
+// matrix constructor
+var Matrix = function (type) {
+  // 内部元属性
+  this.matrixType = type;
+};
+Matrix.prototype.change = function (id, opacity) {
+  matrixManager.setExternalState(this, id);
+  // 更换透明度
+  this.dom.style.opacity = this.opacity;
+};
+
+// matrix factory
+var matrixFactory = (function () {
+  // matrix
+  var matrixOrigin = {};
+
+  return {
+    get: function (type) {
+      if(matrixOrigin[type])
+        return matrixOrigin[type];
+      else
+        return matrixOrigin[type] = new Matrix(type);
     }
-});
+  }
+})();
+
+// matrixManager
+var matrixManager = (function () {
+  // 存储所有martix数据的二维数组
+  var matrixDatabase = [];
+  var matrixLength = 50;
+
+  // 添加matrix数据
+  var matrixAdd = function (id, type, text) {
+
+    var dom = document.createElement('div');
+    dom.setAttribute('class', 'matrix');
+    dom.innerHTML = text;
+    dom.onclick = function () {
+      matrixFactory.get(type).change(id, 0);
+    };
+    document.querySelector('.matrix-border').appendChild(dom);
+
+    var len = matrixDatabase.length;
+    if (matrixDatabase[len - 1] && matrixDatabase[len - 1].length < matrixLength) {
+      matrixDatabase[len - 1].push({
+        id: id,
+        type: type,
+        text: text,
+        opacity: 1,
+        dom: dom
+      })
+    }else {
+      matrixDatabase.push( [{
+        id: id,
+        type: type,
+        text: text
+      }] );
+    }
+  };
+
+  // 组装外部状态
+  var setExternalState = function (obj, id) {
+    console.log(id);
+    // 二维数组的子数组的length
+    var dbLength = matrixLength;
+    var rangerIndex = 0;
+
+    // 存储查找到的区间
+    for(var i = 0; i < matrixDatabase.length; i++){
+      if (id <= i * dbLength) {
+        rangerIndex = i;
+        console.log(rangerIndex);
+        break;
+      }
+    }
+
+    // 查找具体的一个id的数据
+    for(var j = 0; j < matrixDatabase[rangerIndex].length; j++){
+      if (matrixDatabase[i][j].id == id) {
+        console.log(id);
+        Object.keys(matrixDatabase[i][j]).map(function (attr) {
+          obj[attr] = ( matrixDatabase[i][j] )[attr];
+        });
+        break;
+      }
+    }
+
+    return obj;
+  };
+
+  // 外部调用
+  return {
+    add: matrixAdd,
+    setExternalState: setExternalState
+  }
+})();
+
+/* ************************* 创建页面结构 ************************* */
+for(var i = 0; i < 2500; i++){
+  matrixManager.add(i, 'visible', 0);
+}
