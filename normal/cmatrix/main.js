80dab80c9915d6052e25ca5f8dfeb073b3a2fe5c
yang.wei <yang.wei@datatom.com>
2017-09-29 13:12:21 +0800

    feat: add tougn-points

--- normal/cmatrix/main.js
+++ normal/cmatrix/main.js
@@ -1,5 +1,6 @@
 /* ************************* 页面初始化 ************************* */
 var matrixFather = document.querySelector('.matrix-border');
+var matrixLength = 50;
 // clear child
 var childNodes = matrixFather.childNodes;
 var length = childNodes.length;
@@ -13,10 +14,14 @@ var Matrix = function (type) {
   // 内部元属性
   this.matrixType = type;
 };
-Matrix.prototype.change = function (id, opacity) {
+Matrix.prototype.change = function (id) {
   matrixManager.setExternalState(this, id);
   // 更换透明度
-  this.dom.style.opacity = this.opacity;
+  if (this.dom.style.color == 'rgba(1, 1, 1, 0)') {
+    this.dom.style.color = '#14bf49';
+  }else {
+    this.dom.style.color = 'rgba(1, 1, 1, 0)';
+  }
 };
 
 // matrix factory
@@ -47,7 +52,7 @@ var matrixManager = (function () {
     dom.setAttribute('class', 'matrix');
     dom.innerHTML = text;
     dom.onclick = function () {
-      matrixFactory.get(type).change(id, 0);
+      matrixFactory.get(type).change(id);
     };
     document.querySelector('.matrix-border').appendChild(dom);
 
@@ -64,32 +69,31 @@ var matrixManager = (function () {
       matrixDatabase.push( [{
         id: id,
         type: type,
-        text: text
+        text: text,
+        opacity: 1,
+        dom: dom
       }] );
     }
   };
 
   // 组装外部状态
   var setExternalState = function (obj, id) {
-    console.log(id);
     // 二维数组的子数组的length
     var dbLength = matrixLength;
     var rangerIndex = 0;
 
     // 存储查找到的区间
     for(var i = 0; i < matrixDatabase.length; i++){
-      if (id <= i * dbLength) {
+      if (id <= (i + 1) * dbLength) {
         rangerIndex = i;
-        console.log(rangerIndex);
         break;
       }
     }
 
     // 查找具体的一个id的数据
     for(var j = 0; j < matrixDatabase[rangerIndex].length; j++){
-      if (matrixDatabase[i][j].id == id) {
-        console.log(id);
-        Object.keys(matrixDatabase[i][j]).map(function (attr) {
+      if (matrixDatabase[rangerIndex][j].id == id) {
+        Object.keys(matrixDatabase[rangerIndex][j]).map(function (attr) {
           obj[attr] = ( matrixDatabase[i][j] )[attr];
         });
         break;
@@ -99,14 +103,27 @@ var matrixManager = (function () {
     return obj;
   };
 
+  // 定时更改组件状态
+  function matrixAutoChange() {
+    for(var i = 0; i < matrixLength; i++){
+      for(var j = 0; j < matrixDatabase.length; j++){
+        matrixFactory.get(matrixDatabase[j][i].type).change(matrixDatabase[j][i].id);
+      }
+    }
+  }
+
   // 外部调用
   return {
     add: matrixAdd,
-    setExternalState: setExternalState
+    setExternalState: setExternalState,
+    autoChange: matrixAutoChange
   }
 })();
 
 /* ************************* 创建页面结构 ************************* */
 for(var i = 0; i < 2500; i++){
-  matrixManager.add(i, 'visible', 0);
+  matrixManager.add(i, 'visible', '+');
 }
+
+/* ************************* 让矩阵元素流动起来 ************************* */
+matrixManager.autoChange();
