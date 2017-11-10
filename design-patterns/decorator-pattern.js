c2634c7e2a6af6ec861d2db5697b2e20b0a57660
yang.wei <yang.wei@datatom.com>
2017-11-10 15:40:54 +0800

    modified

--- design-patterns/decorator-pattern.js
+++ design-patterns/decorator-pattern.js
@@ -1,6 +1,14 @@
-/*
-传统面向对象语言的装饰者
-*/
+/* -----------------------------------------------------------------------------
+  装饰者模式：
+  对原始操作对象进行层层封装，动态地增加对象的职责，且不改变原始对象自身。
+  装饰者模式和代理模式有一定的相似性，最重要的区别在于他们的意图和设计目的。代理模式的目的是
+  当直接访问本体不方便或者不符合需要时，为这个本体提供一个替代者，本体定义了关键的功能，而代理模式
+  则控制对本体的访问或是对本体的访问做预处理。而装饰者模式的作用就是为对象动态地添加行为，换句话说
+  代理模式体现一种代理和本体之间的关系，这种关系是静态的，已经被预先确定，而装饰者模式使用于一开始
+  不确定对象的全部功能，以黑盒似的将对象的功能表达做层层包裹，所以如果层次太多，会对性能造成影响。
+----------------------------------------------------------------------------- */
+
+/* ************************* 传统面向对象语言的装饰者 ************************* */
 (function () {
 
   var Plane = function () {};
@@ -28,9 +36,7 @@
 
 })();
 
-/*
-JavaScript的装饰者
-*/
+/* ************************* JavaScript的装饰者 ************************* */
 
 // 直接改造原有对象
 (function functionName() {
@@ -66,14 +72,16 @@ JavaScript的装饰者
 
 })();
 
-// 侵入性更改原型方法
+/* ************************* 侵入型更改原型方法 ************************* */
 (function functionName() {
 
   // 函数执行前
   Function.prototype.before = function (beforeFn) {
+    // 保存对原始函数的引用
     var _self = this;
 
     return function () {
+      // 里层this是调用function时的执行环境
       beforeFn.apply(this, arguments);
       _self.apply(this, arguments);
     };
@@ -81,11 +89,13 @@ JavaScript的装饰者
 
   // 函数执行后
   Function.prototype.after = function (afterFn) {
+    // 保存对原始函数的引用
     var _self = this;
 
     return function () {
+      // 里层this是调用function时的执行环境
+      afterFn.apply(this, arguments);
       _self.apply(this, arguments);
-      after.apply(this, arguments);
     };
   };
 
@@ -94,4 +104,18 @@ JavaScript的装饰者
     console.log('正在执行getElementById');
   });
   var button = document.getElementById('buttonId');
+})();
+
+/* ************************* 侵入型改进 ************************* */
+(function () {
+  var before = function (fn, beforeFn) {
+    return function () {
+      beforeFn.apply(this, arguments);
+      fn.apply(this, arguments);
+    }
+  };
+
+  var after = function () {
+
+  };
 })();
--- design-patterns/observer-pattern.js
+++ design-patterns/observer-pattern.js
@@ -1,74 +1,84 @@
+/* -----------------------------------------------------------------------------
+  观察者模式：
+  1. 全局的观察者模式对象，所有事件的订阅、触发、移除都由这个全局对象处理，适用于需要创建大量
+  观察者对象。
+  2. 构建观察者模式类，使用时可以实例化一个观察者对象，适用于需要创建少量的观察者对象。
+----------------------------------------------------------------------------- */
+
+/* ************************* 函数式观察者实现 ************************* */
+// 观察者初始化
+var ObserverObject = {
+  watcherList: {
+    '_default': {}
+  }
+};
+
 /**
- * 观察者模式
+ * [listen 注册新的观察者]
+ * @param paramObj {Object.isRequired} [描述所有参数的对象]
+ *   - type {String.isRequired} [监听的事件名]
+ *   - callback {Func.isRequired} [事件触发的回调函数]
+ *   - namespace {String} [事件触发的命名空间]
  */
-// 01. 函数式观察者实现 //
- // 观察者初始化
- function observerInit(object) {
-     // 定义分数模式的观察者回调函数
-     object.watcherList = {
-         _list: []
-     };
-     // 为分数模式注册新的观察者
-     object.listen = function (fn, type) {
+ObserverObject.listen = function (paramObj) {
+  var type = paramObj['type'],
+    namespace = paramObj['namespace'] || '_default',
+    callback  = paramObj['callback'];
 
-         if(type){
 
-             if(!object.watcherList[type]){
-                 object.watcherList[type] = [fn];
-             }else {
-                 object.watcherList[type].push(fn);
-             }
-         }else {
-             object.watcherList._list.push(fn);
-         }
+  if (!ObserverObject.watcherList[namespace]) {
+    ObserverObject.watcherList[namespace] = {};
+    ObserverObject.watcherList[namespace][type] = [];
+  }
+  if (!type)
+    throw new Error('function listen in ObserverObject needs a param -> type !');
 
-     };
-     // 被观察者事件触发
-     object.trigger = function (type) {
+  ObserverObject.watcherList[namespace][type].push(fn);
+ };
 
-         if(type){
+ /**
+  * [trigger 触发指定类型的所有监听函数]
+  * @param paramObj {Object.isRequired} [描述传入参数的对象]
+  *   - type {String.isRequired} [监听的事件名]
+  *   - data {Object | String} [触发携带的参数]
+  *   - namespace {String} [事件触发的命名空间]
+  */
+object.trigger = function (paramObj) {
+  var type = paramObj['type'],
+    namespace = paramObj['namespace'] || '_default',
+    data = paramObj['data'];
+  var list = ObserverObject.watcherList[namespace][type];
 
-             if(object.watcherList[type]){
-                 for(var i = 0; i < object.watcherList[type].length; i++){
-                     object.watcherList[type][i].apply(object);
-                 }
-             }else {
-                 return false;
-             }
+  if(list && list.length){
+    for(var i = 0; i < list.length; i++){
+      list[i].call(object, data);
+    }
+  }else {
+    return false;
+  }
+ };
 
-         }else {
+ // 解绑观察者事件监听
+object.remove = function (paramObj) {
+  var type = paramObj['type'],
+    namespace = paramObj['namespace'] || '_default',
+    callback = paramObj['callback'];
+  var list = Object.watcherList[namespace][type];
 
-             for(var i = 0; i < object.watcherList._list.length; i++){
-                 object.watcherList._list[i].apply(object);
-             }
-         }
-     };
+  if (!type)
+    throw new Error('function listen in ObserverObject needs a param -> type !');
 
-     // 解绑观察者事件监听
-     object.remove = function (fn, type) {
+  if (!list) return false;
 
-         if(type){
+  for(var i = list.length - 1; i >= 0; i-- ){
+      if(fn === list[i]){
+          list.splice(i, 1);
+      }
+  }
+ };
 
-             var length = object.watcherList[type].length;
-             for(var i = length - 1; i >= 0; i-- ){
-                 if(fn === object.watcherList[type][i]){
-                     object.watcherList[type].splice(i, 1);
-                 }
-             }
-         }else {
 
-             var length = object.watcherList._list.length;
-             for(var i = length - 1; i >= 0; i-- ){
-                 if(fn === object.watcherList._list[i]){
-                     object.watcherList._list.splice(i, 1);
-                 }
-             }
-         }
-
-     };
- }
-
- // 02. 类式观察者实现 //
+ /* ************************* 类式观察者实现 ************************* */
  // 事件对象
 function ObserverTarget() {
   this.handlers = {};
